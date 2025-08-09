4d6da3d8dc65e2764bdf8cdc9d9a53f886b2b7f4
Umut Bütün <umbutun@gmail.com>
2025-08-09 11:57:05 +0000

    Finished hangman project
    Completed adding art and words dictionary and debug for errors

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 #Step 5
 
 import random
-
+from hangman_words import word_list
+from hangman_art import logo, stages
 #TODO-1: - Update the word list to use the 'word_list' from hangman_words.py
 #Delete this line: word_list = ["ardvark", "baboon", "camel"]
 chosen_word = random.choice(word_list)
@@ -11,9 +12,9 @@ end_of_game = False
 lives = 6
 
 #TODO-3: - Import the logo from hangman_art.py and print it at the start of the game.
-
+print(logo)
 #Testing code
-print(f'Pssst, the solution is {chosen_word}.')
+#print(f'Pssst, the solution is {chosen_word}.')
 
 #Create blanks
 display = []
@@ -21,14 +22,16 @@ for _ in range(word_length):
     display += "_"
 
 while not end_of_game:
+    print(f"You have {lives} lives left.")
     guess = input("Guess a letter: ").lower()
 
     #TODO-4: - If the user has entered a letter they've already guessed, print the letter and let them know.
-
+    if guess in chosen_word:
+        print(f"You've already guessed {guess}")
     #Check guessed letter
     for position in range(word_length):
         letter = chosen_word[position]
-        print(f"Current position: {position}\n Current letter: {letter}\n Guessed letter: {guess}")
+        #print(f"Current position: {position}\n Current letter: {letter}\n Guessed letter: {guess}")
         if letter == guess:
             display[position] = letter
 
@@ -36,9 +39,10 @@ while not end_of_game:
     if guess not in chosen_word:
         #TODO-5: - If the letter is not in the chosen_word, print out the letter and let them know it's not in the word.
         lives -= 1
+        print(f"You guessed {guess}, that's not in the word. You lose a life.")
         if lives == 0:
             end_of_game = True
-            print("You lose.")
+            print(f"You lose. The word was {chosen_word}")
 
     #Join all the elements in the list and turn it into a String.
     print(f"{' '.join(display)}")
